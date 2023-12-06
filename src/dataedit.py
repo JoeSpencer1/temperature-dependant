13d99412759526c5be1089ef687e2a16a8b99f95
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2023-12-06 15:53:01 -0700

    Added more coding work to repository.

--- src/dataedit.py
+++ src/dataedit.py
@@ -2,20 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-temperature = '750'
+temperature = '25'
 method = '1'
-n = 28
+n = 25
+max = 400
+mdif = 10 # In 25˚C, this is a little less than 2.
 
 #'''
 WpWt = np.zeros(n)
 dPdh = np.zeros(n)
 C = np.zeros(n)
 dif = np.zeros(n)
+Wpt = np.zeros(n)
+Wet = np.zeros(n)
+Wtt = np.zeros(n)
 #'''
 sy = np.zeros(n)
 
 for i in range(n):
-    '''
+    #'''
     title = '../data/Ti33/' + temperature + 'C/Method ' + method + '_000'
     if i < 10:
         title += '0'
@@ -53,6 +58,9 @@ for i in range(n):
             d = abs(exp['Depth (nm)'][j] - exp['Depth (nm)'][j - 1])
                 
     Wp = Wt - We
+    Wpt[i] = Wp
+    Wet[i] = We
+    Wtt[i] = Wt
     #print(We, ' ', Wp, ' ', Wt)
     WpWt[i] = Wp / Wt
     print('WpWt = ', WpWt[i])
@@ -60,10 +68,10 @@ for i in range(n):
 
     dPdh[i] = 1000 * (exp.loc[blankLines[5] + nsl, 'Load (uN)'] - exp.loc[blankLines[5], 'Load (uN)']) / (exp.loc[blankLines[5] + nsl, 'Depth (nm)'] - exp.loc[blankLines[5], 'Depth (nm)'])
     print('dP/dh = ', dPdh[i])
-    #''''''
+    #'''
     title = '../data/Ti33_' + temperature + 'a.csv'
     res = pd.read_csv(title)
-    #''''''
+    #'''
     res['hmax(nm)'] = pd.to_numeric(res['hmax(nm)'], errors='coerce')
     res['Pmax(uN)'] = pd.to_numeric(res['Pmax(uN)'], errors='coerce')
     #print(res.loc[0, 'hmax(nm)'])
@@ -75,4 +83,11 @@ for i in range(n):
     res['dP/dh (N/m)'] = dPdh
     res['Wp/Wt'] = WpWt
     res['md'] = dif
-    ''' 
+    #'''
+    for i in range(n):
+        sy[i] = 0.83
+    res["sy (GPa)"] = sy
+    res.to_csv(title, index=False)
+print('We: ', Wet)
+print('Wp: ', Wpt)
+print('Wt: ', Wtt)
